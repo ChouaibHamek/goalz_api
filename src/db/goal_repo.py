e82952492e0424aeb40c126466b4dc1163797812
ChouaibHamek <11193161+ChouaibHamek@users.noreply.github.com>
2018-02-23 18:37:31 +0200

    Add goal_repo helper methods
    The _create_goal_object and _create_goal_list_object have been created
    to convert sqlite rows to python dictionary with adequate fields

--- src/db/goal_repo.py
+++ src/db/goal_repo.py
@@ -13,6 +13,39 @@ class GoalRepo(object):
         super(GoalsRepo, self).__init__()
         self.con = con
 
+
+    # HELPER METHODS FOR GOALS
+    def _create_goal_object(self, row):
+        '''
+        It takes a :py:class:`sqlite3.Row` and transform it into a dictionary.
+
+        :param row: The row obtained from the database.
+        :type row: sqlite3.Row
+        :return: a dictionary containing the following keys:
+
+            * ``goal_id``: id of the goal (int)
+            * ``parent_id``: id of the parent goal (int)
+            * ``user_id``: id of the user associated with the goal (int)
+            * ``title``: goal's title (string)
+            * ``topic``: goal's topic (string)
+            * ``description``: goal's description (string)
+            * ``deadline``: goal's set deadline (int)
+            * ``status``: goal's completion status (int)
+
+        '''
+        print("inside _create_goal_object")
+        goals = {}
+        for key in row.keys():
+            goals[key] = row[key]
+        return goals
+
+    def _create_goal_list_object(self, row):
+        return {
+            'goal_id': row['goal_id'],
+            'title': row['title'],
+            'description': row[description]
+        }
+
     def get_goal(self, goal_id):
         raise NotImplementedError("")
 
